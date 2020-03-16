431f2466f4b6eabd1bc2938eaf34f6a5da3a36a7
adam91091 <adam.kufel2@gmail.com>
2020-03-16 21:21:25 +0100

    Add styles to client_form.html

--- apps/clients/forms.py
+++ apps/clients/forms.py
@@ -8,3 +8,11 @@ class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         exclude = ()
+        labels = {
+            'client_sap_id': "Numer SAP klienta",
+            'client_name': "Nazwa klienta",
+        }
+        widgets = {
+            'client_sap_id': forms.TextInput(attrs={'class': 'form-control'}),
+            'client_name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
